ca7df3f052d428686aabe347cdc09f9a3dfd671b
Aditya Mitra <adityaarghya0@gmail.com>
2022-03-02 21:14:24 +0530

    Update storageoperations.py

--- storageoperations.py
+++ storageoperations.py
@@ -5,15 +5,6 @@ connect_str='DefaultEndpointsProtocol=https;AccountName=paymentkeys;AccountKey=p
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
 cryptocontainer=blob_service_client.get_container_client('cryptofiles')
-usercontainer=blob_service_client.get_container_client('userfiles')
-
-def uploadUserFileToBlob(data, fln):
-  blob_client = blob_service_client.get_blob_client(container='userfiles', blob=fln)
-  blob_client.upload_blob(data)
-  
-def getDownloadLink(fln):
-  blob_client = blob_service_client.get_blob_client(container='userfiles', blob=fln)
-  return blob_client.download_blob().readall()
 
 def uploadCryptoFile(data,fln):
   blob_client = blob_service_client.get_blob_client(container='cryptofiles', blob=fln)
@@ -28,18 +19,11 @@ def createContainers():
     container_properties = cryptocontainer.get_container_properties()
   except Exception as e:
     cryptocontainer.create_container()
-  try:
-    container_properties = usercontainer.get_container_properties()
-  except Exception as e:
-    usercontainer.create_container()
+ 
     
 def resetContainers():
   try:
     cryptocontainer.delete_container()
   except Exception as e:
     pass
-  try:
-    usercontainer.delete_container()
-  except Exception as e:
-    pass
-  
+ 
